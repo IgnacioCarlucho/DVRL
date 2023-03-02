e1835b59d7ea55873e73687c967e0b1f2adf242d
ignaciocarlucho <ignacio.carlucho@gmail.com>
2023-03-02 17:37:12 +0000

    added runner for lbf

---
+++ run_lbf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=cook_qmix
+#SBATCH --cpus-per-task=4
+#SBATCH --time=8-24:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gres=gpu:1
+
+source activate dvrl
+
+export OMP_NUM_THREADS=1 
+
+# export CUDA_VISIBLE_DEVICES="0"
+
+python ./code/main.py -p with environment.config_file=lbf.yaml
