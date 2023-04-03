7e0faf46131f8da5a4b0e21834fcbd31f0b29b03
ignaciocarlucho <ignacio.carlucho@gmail.com>
2023-04-03 17:34:27 +0100

    update to eval run

--- eval_wolf.sh
+++ eval_wolf.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+#SBATCH --job-name=dvrl_lbf
+#SBATCH --cpus-per-task=4
+#SBATCH --time=2-24:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gres=gpu:1
 
 # Evaluate linear with colapse with smaller learning rate 
-python ./code/ckpt_main.py -p with environment.config_file=wolf.yaml seed=123 load.run=30 log.test_dir=wolf
-python ./code/ckpt_main.py -p with environment.config_file=wolf.yaml seed=445 load.run=31 log.test_dir=wolf
-python ./code/ckpt_main.py -p with environment.config_file=wolf.yaml seed=7881 load.run=32 log.test_dir=wolf
-python ./code/ckpt_main.py -p with environment.config_file=wolf.yaml seed=1116 load.run=33 log.test_dir=wolf
+# python ./code/ckpt_main.py -p with environment.config_file=wolf.yaml seed=123 load.run=20 log.test_dir=wolf
+# python ./code/ckpt_main.py -p with environment.config_file=wolf.yaml seed=445 load.run=21 log.test_dir=wolf
+# python ./code/ckpt_main.py -p with environment.config_file=wolf.yaml seed=7881 load.run=22 log.test_dir=wolf
+# python ./code/ckpt_main.py -p with environment.config_file=wolf.yaml seed=1116 load.run=23 log.test_dir=wolf
+
+# python ./code/ckpt_main.py -p with environment.config_file=wolf_test.yaml seed=123 load.run=20 log.test_dir=wolf
+# python ./code/ckpt_main.py -p with environment.config_file=wolf_test.yaml seed=445 load.run=21 log.test_dir=wolf
+# python ./code/ckpt_main.py -p with environment.config_file=wolf_test.yaml seed=7881 load.run=22 log.test_dir=wolf
+# python ./code/ckpt_main.py -p with environment.config_file=wolf_test.yaml seed=1116 load.run=23 log.test_dir=wolf
