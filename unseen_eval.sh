30ecad670b1132fd0025c3dc5c890b6efc20150f
ignaciocarlucho <ignacio.carlucho@gmail.com>
2023-05-05 19:40:28 +0100

    fix

--- unseen_eval.sh
+++ unseen_eval.sh
@@ -1,4 +1,48 @@
 #!/bin/bash
+#SBATCH --job-name=dvrl_lbf
+#SBATCH --cpus-per-task=4
+#SBATCH --time=4-24:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gres=gpu:1
+
+source activate dvrl
+
+
+
+
+
+
+
+
+# For server 
+python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=123 load.run=20 log.test_dir=saved_runs log.checkpoint=[30,40]
+python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=123 load.run=21 log.test_dir=saved_runs log.checkpoint=[30,40]
+python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=445 load.run=22 log.test_dir=saved_runs log.checkpoint=[30,40]
+python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=7881 load.run=23 log.test_dir=saved_runs log.checkpoint=[30,40]
+python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=6673 load.run=24 log.test_dir=saved_runs log.checkpoint=[30,40]
+python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=84145 load.run=25 log.test_dir=saved_runs log.checkpoint=[30,40]
+python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=9911 load.run=26 log.test_dir=saved_runs log.checkpoint=[30,40]
+python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=1026 load.run=27 log.test_dir=saved_runs log.checkpoint=[30,40]
+
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=123 load.run=20 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=123 load.run=21 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=445 load.run=22 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=7881 load.run=23 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=6673 load.run=24 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=84145 load.run=25 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=9911 load.run=26 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=1026 load.run=27 log.test_dir=wolf log.checkpoint=[30,40]
+
+
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=123 load.run=20 log.test_dir=wolf  log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=123 load.run=21 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=445 load.run=22 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=7881 load.run=23 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=6673 load.run=24 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=84145 load.run=25 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=9911 load.run=26 log.test_dir=wolf log.checkpoint=[30,40]
+# python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=1026 load.run=27 log.test_dir=wolf log.checkpoint=[30,40]
+
 
 
 # # Evaluate train
@@ -48,26 +92,3 @@
 # python ./code/single_ckpoint.py -p with environment.config_file=coop_test.yaml seed=1026 load.run=53 log.test_dir=coop log.checkpoint=[36,40]
 
 
-
-
-python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=123 load.run=20 log.test_dir=wolf  log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=123 load.run=21 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=445 load.run=22 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=7881 load.run=23 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=6673 load.run=24 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=84145 load.run=25 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=9911 load.run=26 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf.yaml seed=1026 load.run=27 log.test_dir=wolf log.checkpoint=[30,40]
-
-
-
-
-# Evaluate test
-python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=123 load.run=20 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=123 load.run=21 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=445 load.run=22 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=7881 load.run=23 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=6673 load.run=24 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=84145 load.run=25 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=9911 load.run=26 log.test_dir=wolf log.checkpoint=[30,40]
-python ./code/single_ckpoint.py -p with environment.config_file=wolf_test.yaml seed=1026 load.run=27 log.test_dir=wolf log.checkpoint=[30,40]
